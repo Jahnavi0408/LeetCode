f1fce9e410ed127293193d749ab81dbbaef90859
Jahnavi0408 <98768175+Jahnavi0408@users.noreply.github.com>
2025-03-03 23:44:44 -0500

    Time: 7 ms (57.14%), Space: 17.8 MB (80.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,10 +1,26 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
 
-        c1 = [s.index(char) for char in s]
-        c2 = [t.index(char) for char in t]
+        # c1 = [s.index(char) for char in s]
+        # c2 = [t.index(char) for char in t]
 
-        return c1 == c2 
+        # return c1 == c2 
+
+        mapst = {}
+        mapts = {}
+
+        for chs, cht in zip(s,t):
+            if chs in mapst and mapst[chs] != cht:
+                return False
+            if cht in mapts and mapts[cht] != chs:
+                return False
+
+            mapst[chs] = cht
+            mapts[cht] = chs
+
+        return True
+            
+        
           
         
 
