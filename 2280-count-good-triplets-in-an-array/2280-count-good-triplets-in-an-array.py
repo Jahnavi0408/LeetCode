10d73854e1fd4cf6faf466584f7e09a0e981afc5
Jahnavi0408 <98768175+Jahnavi0408@users.noreply.github.com>
2025-04-15 10:22:46 -0400

    Time: 1007 ms (53.77%), Space: 48.6 MB (32.08%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
@@ -10,7 +10,7 @@ class BIT:
             index += index & -index
     
     def query(self, index):
-        # sum from 0 to index (inclusive)
+       
         index += 1
         res = 0
         while index > 0:
@@ -21,27 +21,25 @@ class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         
-        # Step 1: position mapping from nums2
+
         pos = {v: i for i, v in enumerate(nums2)}
-        
-        # Step 2: map nums1 to positions in nums2
+
         A = [pos[val] for val in nums1]
 
-        # Step 3: count of smaller to the left
         left_BIT = BIT(n)
         left_smaller = [0] * n
         for i in range(n):
             left_smaller[i] = left_BIT.query(A[i] - 1)
             left_BIT.update(A[i], 1)
         
-        # Step 4: count of greater to the right
+
         right_BIT = BIT(n)
         right_greater = [0] * n
         for i in range(n - 1, -1, -1):
             right_greater[i] = right_BIT.query(n - 1) - right_BIT.query(A[i])
             right_BIT.update(A[i], 1)
         
-        # Step 5: total good triplets
+
         ans = 0
         for i in range(n):
             ans += left_smaller[i] * right_greater[i]
